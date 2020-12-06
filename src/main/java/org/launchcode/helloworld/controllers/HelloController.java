705a58987cb5d4d8b057b0b2be1136aacea626a4
Aby <abyssiniaasfaw@gmail.com>
2020-12-06 13:32:40 -0600

    Forgot Password Form

--- src/main/java/org/launchcode/helloworld/controllers/HelloController.java
+++ src/main/java/org/launchcode/helloworld/controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
 
-    @RequestMapping("")
+    @RequestMapping("/index")
     public String index() {
 
         return "index";
